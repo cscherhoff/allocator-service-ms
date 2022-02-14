6cecf496e73968c293c9fefb3ae3bedcb30d1857
cesc <cscherhoff@web.de>
2022-02-14 21:51:11 +0100

    Initial commit

---
+++ src/main/java/com/exxeta/allocatorservicems/AllocatorServiceMsApplication.java
@@ -0,0 +1,13 @@
+package com.exxeta.allocatorservicems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllocatorServiceMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllocatorServiceMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exxeta/allocatorservicems/AllocatorServiceMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exxeta.allocatorservicems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllocatorServiceMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
