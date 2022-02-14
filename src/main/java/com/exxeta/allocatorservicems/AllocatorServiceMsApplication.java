b4fd5db07d232dd9a7748fbd865a9702e458ba13
cesc <cscherhoff@web.de>
2022-02-14 23:13:43 +0100

    Add database information

--- src/main/java/com/exxeta/allocatorservicems/AllocatorServiceMsApplication.java
+++ src/main/java/com/exxeta/allocatorservicems/AllocatorServiceMsApplication.java
@@ -3,7 +3,7 @@ package com.exxeta.allocatorservicems;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.exxeta")
 public class AllocatorServiceMsApplication {
 
 	public static void main(String[] args) {
