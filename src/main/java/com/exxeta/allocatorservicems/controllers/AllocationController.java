e54e9f6c09af88ba0432d53f593a087400b16430
cesc <cscherhoff@web.de>
2022-02-26 14:38:03 +0100

    Update controller to match the underlying service and fix KafkaHandler

--- src/main/java/com/exxeta/allocatorservicems/controllers/AllocationController.java
+++ src/main/java/com/exxeta/allocatorservicems/controllers/AllocationController.java
@@ -2,14 +2,15 @@ package com.exxeta.allocatorservicems.controllers;
 
 import com.exxeta.allocatorservice.entities.Allocation;
 import com.exxeta.allocatorservice.services.AllocationService;
+import com.exxeta.allocatorservice.services.InvalidAllocationException;
 import com.exxeta.allocatorservicems.kafka.KafkaHandler;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(path = "/allocation/user/{userId}")
@@ -26,14 +27,18 @@ public class AllocationController {
     }
 
     @GetMapping
-    public String getAllocations(@PathVariable long userId) throws JsonProcessingException {
+    public String getAllocation(@PathVariable long userId) throws JsonProcessingException {
         logger.info("Getting the allocations from database");
-        return mapper.writeValueAsString(allocationService.getAllocations(userId));
+        return mapper.writeValueAsString(allocationService.getAllocation(userId));
     }
 
     @PutMapping
-    public void updateAllocations(@PathVariable long userId, @RequestBody List<Allocation> allocations) {
-        allocationService.updateAllocations(userId, allocations);
-        kafkaHandler.sendMessages(userId, allocations);
+    public void updateAllocation(@PathVariable long userId, @RequestBody Allocation allocation) {
+        try {
+            allocationService.updateAllocation(userId, allocation);
+            kafkaHandler.publishAllocationUpdate(userId, allocation);
+        } catch (InvalidAllocationException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
     }
 }
--- src/main/java/com/exxeta/allocatorservicems/kafka/KafkaHandler.java
+++ src/main/java/com/exxeta/allocatorservicems/kafka/KafkaHandler.java
@@ -2,18 +2,15 @@ package com.exxeta.allocatorservicems.kafka;
 
 import com.exxeta.allocatorservice.entities.Allocation;
 import com.exxeta.allocatorservice.repositories.AllocationRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import java.math.BigDecimal;
 
 @Service
 public class KafkaHandler {
     private final KafkaTemplate<String, String> kafkaTemplate;
-    private final ObjectMapper mapper = new ObjectMapper();
     private final AllocationRepository allocationRepository;
 
     public KafkaHandler(KafkaTemplate<String, String> kafkaTemplate, AllocationRepository allocationRepository) {
@@ -24,41 +21,24 @@ public class KafkaHandler {
     /**
      * publishes a Kafka message for every destination where the allocation has changed
      */
-    public void sendMessages(long userId, List<Allocation> allocationsFromRequest) {
-        for (Allocation allocation: allocationsFromRequest) {
-            if (amountHasChanged(allocation)) {
-                sendMessages(userId, allocation);
-            }
+    public void publishAllocationUpdate(long userId, Allocation allocationFromRequest) {
+        final Allocation allocationFromDb = allocationRepository.findById(userId).orElseGet(() -> new Allocation(userId));
+        if (valueHasChanged(allocationFromRequest.getInvestment(), allocationFromDb.getInvestment())) {
+            sendMessage(userId, "investment", allocationFromRequest.getInvestment());
+        }
+        if (valueHasChanged(allocationFromRequest.getFixCosts(), allocationFromDb.getFixCosts())) {
+            sendMessage(userId, "fixCosts", allocationFromRequest.getFixCosts());
+        }
+        if (valueHasChanged(allocationFromRequest.getCategories(), allocationFromDb.getCategories())) {
+            sendMessage(userId, "categories", allocationFromRequest.getCategories());
         }
     }
 
-    private boolean amountHasChanged(Allocation allocationFromRequest) {
-        final Optional<Allocation> optionalAllocation = allocationRepository.findByDestination(allocationFromRequest.getDestination());
-        if (optionalAllocation.isEmpty()) {
-            return true;
-        }
-        return optionalAllocation.get().getAmount().doubleValue() == allocationFromRequest.getAmount().doubleValue();
+    private void sendMessage(long userId, String topic, BigDecimal newValue) {
+        kafkaTemplate.send(topic, "The new value for the user " + userId + " is " + newValue);
     }
 
-    public void sendMessages(long userId, Allocation allocation) {
-        try {
-            String topic = getTopic(allocation);
-            allocation.setUserId(userId);
-            kafkaTemplate.send(topic, mapper.writeValueAsString(allocation));
-        } catch (UnknownDestinationException | JsonProcessingException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private String getTopic(Allocation allocation) throws UnknownDestinationException {
-        switch (allocation.getDestination()) {
-            case "investment":
-                return "investment";
-            case "categories":
-                return "categories";
-            case "fixcosts":
-                return "fixcosts";
-        }
-        throw new UnknownDestinationException("The destination " + allocation.getDestination() + " is unknown");
+    private boolean valueHasChanged(BigDecimal valueFromRequest, BigDecimal valueFromDb) {
+        return valueFromRequest.doubleValue() != valueFromDb.doubleValue();
     }
 }
