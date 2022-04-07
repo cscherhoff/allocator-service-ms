b0aafba8c5a6d279b41c08e41b0c00d0d21c35f2
cesc <cscherhoff@web.de>
2022-04-07 22:06:44 +0200

    Use eureka and feign

--- src/main/java/com/exxeta/allocatorservicems/AllocatorServiceMsApplication.java
+++ src/main/java/com/exxeta/allocatorservicems/AllocatorServiceMsApplication.java
@@ -2,8 +2,10 @@ package com.exxeta.allocatorservicems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = "com.exxeta")
+@EnableFeignClients
 public class AllocatorServiceMsApplication {
 
 	public static void main(String[] args) {
