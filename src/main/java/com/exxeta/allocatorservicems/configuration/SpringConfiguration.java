c214e1259974fb60c1148853ea5e1b2e85e5ffeb
Cedric <cscherhoff@web.de>
2025-04-02 18:42:02 +0200

    Update CORS configuration

--- src/main/java/com/exxeta/allocatorservicems/configuration/SpringConfiguration.java
+++ src/main/java/com/exxeta/allocatorservicems/configuration/SpringConfiguration.java
@@ -13,7 +13,13 @@ public class SpringConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT");
+        registry.addMapping("/**")
+                .allowedOriginPatterns(
+                        "http://localhost:*",
+                        "http://192.168.*.*"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
